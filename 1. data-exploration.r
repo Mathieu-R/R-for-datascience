b1edc6ba5079ae821ebff8a49fdd2a4d33cc8395
Mathieu-R <mathieu0709@gmail.com>
2022-04-14 17:11:37 +0200

    first commit

---
+++ 1. data-exploration.r
@@ -0,0 +1,14 @@
+library(tidyverse, help, pos = 2, lib.loc = NULL)
+
+# Question: do cars with big engines consume more fuel than cars with smaller engine ?
+# To answer that question, we use "mpg" dataset.
+
+# displ: car engine size (in liter)
+# hwy: car engine efficiency on highway (more is better)
+mpg
+
+# ggplot(data = <DATA>) + 
+#  <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x=displ, y=hwy, color=class))
+
